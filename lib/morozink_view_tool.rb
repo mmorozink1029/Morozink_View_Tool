1c624710987b31edaa733ca2c2b45598b23bf946
mmorozink1029 <mattmorozink@gmail.com>
2018-01-31 14:50:54 -0700

    Morozink tool intial commit

---
+++ lib/morozink_view_tool.rb
@@ -0,0 +1,6 @@
+require "morozink_view_tool/version"
+require "morozink_view_tool/renderer"
+
+module MorozinkViewTool
+  # Your code goes here...
+end
---
+++ lib/morozink_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MorozinkViewTool
+  VERSION = "0.1.0"
+end
